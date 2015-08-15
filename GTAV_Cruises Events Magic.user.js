2cb23dd2e402d296d5e629d72e223183221a203d
Justin Howe <justinhowe@outlook.com>
2015-08-14 20:28:32 -0700

    test
    test

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GTAV_Cruises Events Magic
 // @namespace    http://your.homepage/
-// @version      0.1
+// @version      0.2
 // @description  enter something useful
 // @author       You
 // @match        https://www.reddit.com/r/GTAV_Cruises/?count=26&before=t3_3govai
