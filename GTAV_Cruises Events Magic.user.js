18701815e22eee984c6c44dd30969370deeb4f7b
Justin Howe <justinhowe@outlook.com>
2015-08-15 00:09:35 -0700

    gta

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GTAV_Cruises Events Magic
 // @namespace    https://github.com/JustinHowe/userscripts/
-// @version      0.75
+// @version      0.76
 // @description  Events block for GTAV_Cruises
 // @author       Syntaximus
 // @match        https://www.reddit.com/r/GTAV_Cruises/
@@ -13,12 +13,25 @@
 
 // Run everything after page load.
 $(window).load(function(){
-	var eventsString;
-	var eventTitle;
-	var events = $x("//span[@title='Event']/..//a[contains(@class, 'title')]");
-	for (var i=0; i < events.length; i++) {
-		eventTitle = events[i].innerHTML;
-		eventsString = eventsString + '<a href="http://www.google.com" target="_blank">' + eventTitle + '</a><br /><br />'
-	}
-	$( ".md" ).prepend( '<blockquote><h3>Upcoming Cruises</h3><p>' + eventsString + '</p></blockquote>' );
-})
+
+	var upcomingEventsLink = "https://www.reddit.com/r/GTAV_Cruises/search?q=flair%3A%22events%22&restrict_sr=on&sort=new&t=all#res-hide-options";
+	var iframe = document.createElement('iframe');
+	iframe.frameBorder=0;
+	iframe.width="0px";
+	iframe.height="0px";
+	iframe.id="eventsiFrame";
+	iframe.setAttribute("src", upcomingEventsLink);
+	$( "#header" ).prepend(iframe);
+
+	$("#eventsiFrame").load(function(){
+
+		var eventsString = "";
+		var eventTitle;
+		var events = $("#eventsiFrame").contents().find("span").filter(function() { return ($(this).text() === 'Event') }).next();
+		for (var i=0; i < events.length; i++) {
+			eventTitle = events[i].innerHTML;
+			eventsString = eventsString + '<a href="http://www.google.com" target="_blank">' + eventTitle + '</a><br /><br />'
+		}
+		$( ".md" ).prepend( '<blockquote><h3>Upcoming Cruises</h3><p>' + eventsString + '</p></blockquote>' );
+	});
+});
