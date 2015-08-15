4b73c20c03aeee9bca57a59edd87e4812c4f62ac
Justin Howe <justinhowe@outlook.com>
2015-08-14 22:55:56 -0700

    gta

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GTAV_Cruises Events Magic
 // @namespace    https://github.com/JustinHowe/userscripts/
-// @version      0.73
+// @version      0.74
 // @description  Events block for GTAV_Cruises
 // @author       Syntaximus
 // @match        https://www.reddit.com/r/GTAV_Cruises/
@@ -11,8 +11,14 @@
 
 // Event Title Format: [Region] | [Date] | [Title] | [Timezone] | [Time]
 
-// Run everything as soon as the DOM is set up.
+// Run everything after page load.
 $(window).load(function(){
-
-	$( "#siteTable" ).prepend( '<h3>Upcoming Cruises</h3><p><a href="http://www.google.com" target="_blank">Event 1</a></p>' );
+	var eventsString;
+	var eventTitle;
+	var events = $x("//span[@title='Event']/..//a[contains(@class, 'title')]");
+	for (var i=0; i < events.length; i++) {
+		eventTitle = events[i].innerHTML;
+		eventsString = eventsString + '<a href="http://www.google.com" target="_blank">' + eventTitle + '</a><br /><br />'
+	}
+	$( ".usertext-body may-blank-within md-container " ).prepend( '<div class="md"><blockquote><h3>Upcoming Cruises</h3><p>' + eventsString + '</p></blockquote></div>' );
 })
