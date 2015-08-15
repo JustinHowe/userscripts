0a0ba3d0b53875ad76aa550c68cd967532b25ce6
Justin Howe <justinhowe@outlook.com>
2015-08-14 22:16:42 -0700

    GTA

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GTAV_Cruises Events Magic
 // @namespace    https://github.com/JustinHowe/userscripts/
-// @version      0.71
+// @version      0.72
 // @description  Events block for GTAV_Cruises
 // @author       Syntaximus
 // @match        https://www.reddit.com/r/GTAV_Cruises/
@@ -17,5 +17,5 @@ if ( ! ( 'jQuery' in window ) ) { return false; }
 // Run everything as soon as the DOM is set up.
 jQuery( document ).ready(function( $ ) {
 
-	$( '<h3>Upcoming Cruises</h3><p><a href="http://www.google.com" target="_blank">Event 1</a></p>' ).insertBefore( "<h3>Welcome</h3>" );
+	$( "#siteTable" ).prepend( '<h3>Upcoming Cruises</h3><p><a href="http://www.google.com" target="_blank">Event 1</a></p>' );
 }
