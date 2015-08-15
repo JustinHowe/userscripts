e319ec62203545349dbab832f4dc56ae127d9874
Justin Howe <justinhowe@outlook.com>
2015-08-15 15:22:32 -0700

    gta

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -17,7 +17,8 @@ $(window).load(function(){
 	var timezone = jstz.determine();
 	var currentTimezone = timezone.name(); 
 	var currentLocation = currentTimezone.split("/");
-	currentLocation = currentLocation[1].replace(/\_/, "+");
+	currentLocation = currentLocation[1].replace(/\_/g, "+");
+	var getCountdownHref;
 	var upcomingEventsLink = "https://www.reddit.com/r/GTAV_Cruises/search?q=flair%3A%22events%22&restrict_sr=on&sort=new&t=all#res-hide-options";
 	var iframe = document.createElement('iframe');
 	iframe.frameBorder=0;
@@ -36,13 +37,88 @@ $(window).load(function(){
 			var href = $(events[i]).attr('href');
 			var eventParts = eventString.split(" | ");
 			var region = eventParts[0];
-			var date = eventParts[1];
-			var title = eventParts[2];
-			var timezone = eventParts[3];
-			var time = eventParts[4];
 
-			eventsString = eventsString + '<strong><a href="' + href + '" target="_blank">'+ title + '</a></strong><br /><br />';
+			//Determine date parts
+			var date = eventParts[1];
+			date = eventParts[1].replace(/\-/g, "/");
+			date = eventParts[1].replace(/\|/g, "/");
+			date = eventParts[1].split("/");
+			var day = parseInt(date[0], 10);
+			var month = parseInt(date[1], 10);
+			var year = parseInt(date[2], 10);
+
+			var title = eventParts[2];
+
+			//Log original time and timezone
+			console.log(title + " - " + eventParts[4] + " - " + day + "/" + month + "/" + year + " - " + eventParts[3]);
+
+			//Convert time to four-digit military time of GMT 00:00 timezone.
+			var time = eventParts[4].split(":");
+			var hour = time[0];
+			var minute = time[1];
+
+			var timezone = eventParts[3];
+			if (timezone.toLowerCase().indexOf("pst") >= 0) {
+				timezone = "GMT-8";
+			}
+			if (timezone.toLowerCase().indexOf("pdt") >= 0) {
+				timezone = "GMT-7";
+			}
+			if (timezone.toLowerCase().indexOf("est") >= 0) {
+				timezone = "GMT-5";
+			}
+			if (timezone.toLowerCase().indexOf("edt") >= 0) {
+				timezone = "GMT-4";
+			}
+			if (timezone.toLowerCase().indexOf("cst") >= 0) {
+				timezone = "GMT-6";
+			}
+			if (timezone.toLowerCase().indexOf("cdt") >= 0) {
+				timezone = "GMT-5";
+			}
+
+			timezone = timezone.replace(/ /g, "");
+			var substringBoundry = timezone.length;
+			var timezoneOffsetHours = parseInt(timezone.substring(4,substringBoundry), 10);
+			var convertedHour;
+
+/*			if (timezone.indexOf("-") >= 0) {
+				convertedHour = hour + timezoneOffsetHours;
+			}
+			if (timezone.indexOf("+") >= 0) {
+				convertedHour = hour - timezoneOffsetHours;
+			}
+
+			if (convertedHour >= 24) {
+				convertedHour = convertedHour - 24;
+				day++;
+
+				var daysInMonth = 31;
+				if ([9,4,6,11].indexOf(month) >=0) {
+					daysInMonth = 30;
+				}
+				if (month == 2) {
+					daysInMonth = 28;
+				}
+
+				if (day > daysInMonth) {
+					day = 1;
+					month++;
+
+					if (month > 12) {
+						month = 1;
+					}
+				}
+		
+			}*/
+
+			//Log new base time and timezone
+			//console.log("Converted - " + title + " - " + convertedHour + ":" + minute + " - " + day + "/" + month + "/" + year);
+
+			//getCountdownHref = "http://www.timeanddate.com/scripts/gocountdown.php?theme=text&msg=" + title.replace(/ /g, "+") + "&font=sanserif&month=" + month + "&day=" + day + "&year=" + year + "&hour=" + convertedHour + "&min=" + minute + "&sec=0&p0=%3A&p0txt=" + currentLocation;
+
+			eventsString = eventsString + '<strong><a href="' + href + '" target="_blank">'+ title.substring(0,30) + '</a></strong>' + month + '/' + day + ' @ ' + time + '<br /><br />';
 		}
-		$(".md").prepend('<blockquote><h3>Upcoming Cruises (' + events.length + ')</h3><p>' + eventsString + '<center><strong>Your timezone location: ' + currentLocation.replace(/\+/, " ") + '</strong></center></p></blockquote>');
+		$(".md").prepend('<blockquote><h3>Upcoming Cruises (' + events.length + ')</h3><p>' + eventsString + '<center><strong>Your timezone location: ' + currentLocation.replace(/\+/g, " ") + '</strong></center></p></blockquote>');
 	})
 })
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Userscripts
 
 Install either Tampermonkey for Chrome, or Greasemonkey for Firefox, to run these scripts. Microsoft Edge (IE on Windows 10) will supposedly also support these userscripts soon.
+
+GTAV_Cruises Events Magic
+This script places a box in the right sidebar that shows all upcoming cruises marked with the "event" flare. The box is titled, "Upcoming Cruises" and is placed above the "Welcome" box after page load. In order to properly work, events must be recorded with proper timezones (preferably GMT, but if PST/PDT/EST/EDT/CST/CDT you better know which one you're currently in and use it right). User error on recording events with non-standard timezones and times will negatively affect that event's display on the userscript box. 
