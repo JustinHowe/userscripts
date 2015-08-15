16c3d5a5affabdab4eee4e3e54c0078d67909de3
Justin Howe <justinhowe@outlook.com>
2015-08-14 19:26:59 -0700

    Adding Userscript Shell
    Creating the shell for the userscript.

---
+++ GTAV_Cruises Events Magic.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         GTAV_Cruises Events Magic
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        https://www.reddit.com/r/GTAV_Cruises/?count=26&before=t3_3govai
+// @grant        none
+// ==/UserScript==
+
