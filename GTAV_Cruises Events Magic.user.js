6ab8b045eb13a248e77d9884685c04af2c8f9b23
Justin Howe <justinhowe@outlook.com>
2015-08-19 01:49:08 -0700

    gta

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -9,18 +9,48 @@
 // @match        https://www.reddit.com/r/gtav_cruises/
 // @match        https://www.reddit.com/r/gtav_cruises
 // @grant        none
-// @require      http://cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.4/jstz.min.js
+// @require      https://github.com/JustinHowe/userscripts/raw/master/jstz.min.js
+// @require      https://github.com/JustinHowe/userscripts/raw/master/jquery.xdomainajax.js
 // ==/UserScript==
 
 //[Region] | [Date] | [Title] | [GMT] | [Time]
 
+var countdowns = [];
+var dates = [];
+var times = [];
+
+console.log = function() {} //Comment to enable console logging. 
+
+function toTitleCase(str) {
+	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function timerUpdate(n) {
+	var timerString = "timer" + n;
+	if (!isNaN(countdowns[n])) {
+		s = countdowns[n]%60;
+		m = (countdowns[n]-s)/60%60;
+		h = ((countdowns[n]-s)/60 - m)/60%24;
+		d = (((countdowns[n]-s)/60 - m)/60 - h)/24;
+
+		if (d > 0) {
+			var txt = d + " Days, " + h +" Hrs, " + m + " Min";
+		} else {
+			var txt = h +" Hrs, " + m + " Min";
+		}
+		document.getElementById(timerString).innerHTML = txt;
+	} else {
+		document.getElementById(timerString).innerHTML = dates[n] + ' @ ' + times[n];
+	}
+}
+
 // Set up the iFrame for all upcoming events after page load.
 $(window).load(function(){
-	var timezone = jstz.determine();
-	var currentTimezone = timezone.name(); 
+	var jstzTimezone = jstz.determine();
+	var currentTimezone = jstzTimezone.name(); 
 	var currentLocation = currentTimezone.split("/");
 	currentLocation = currentLocation[1].replace(/\_/g, "+");
-	var getCountdownHref;
+	var countdownHref;
 	var upcomingEventsLink = "https://www.reddit.com/r/GTAV_Cruises/search?q=flair%3A%22events%22&restrict_sr=on&sort=new&t=all#res-hide-options";
 	var iframe = document.createElement('iframe');
 	iframe.frameBorder=0;
@@ -35,6 +65,7 @@ $(window).load(function(){
 		var eventsString = "";
 		var events = $("#eventsiFrame").contents().find("header.search-result-header > span").filter(function() { return ($(this).text() === 'Event') }).next();
 		for (var i=0; i < events.length; i++) {
+			console.log("Events Found: " + events.length);
 			var eventString = events[i].innerHTML;
 			var wellFormedEvent = eventString.match(/\|/g);
 			if (wellFormedEvent.length == 4) {
@@ -44,98 +75,151 @@ $(window).load(function(){
 
 				//Determine date parts
 				var date = eventParts[1];
+				dates[i] = eventParts[1];
 				date = eventParts[1].replace(/\-/g, "/");
-				date = eventParts[1].split("/");
-				var day = parseInt(date[0], 10);
-				var month = parseInt(date[1], 10);
-				var year = parseInt(date[2], 10);
+				var day;
+				var month;
+				var year;
+				if (date.indexOf("/") >= 0) {
+					date = eventParts[1].split("/");
+					day = parseInt(date[0], 10);
+					month = parseInt(date[1], 10);
+					year = date[2];
+					var yearFirstChar = year.charAt(0);
 
-				var title = eventParts[2];
-				function toTitleCase(str) {
-					return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+					if (yearFirstChar != "2") {
+						year = "20" + year;
+					}
+
+					year = parseInt(year, 10);
 				}
-				title = toTitleCase(title);
+				
+				//var title = toTitleCase(eventParts[2]); //Convert to lowercast starting with 2nd character of each word
+				var title = eventParts[2];
+				var titleShort;
 
 				//Log original time and timezone
 				console.log(title + " - " + eventParts[4] + " - " + day + "/" + month + "/" + year + " - " + eventParts[3]);
 
-				//Convert time to four-digit military time of GMT 00:00 timezone.
+				//Convert to four-digit military time and UTC time zone.
 				var time = eventParts[4].split(":");
+				times[i] = eventParts[4];
 				var hour = time[0];
 				var minute = time[1];
+				hour = hour.replace(/ /g, "");
+				hour = parseInt(hour, 10);
+				minute = minute.replace(/ /g, "");
+				minute = parseInt(minute, 10);
+
+				if (times[i].toLowerCase().indexOf("pm") >= 0) {
+					hour = hour + 12;
+				}
+
+				console.log("24hr Hour: " + hour);
 
 				var timezone = eventParts[3];
 				if (timezone.toLowerCase().indexOf("pst") >= 0) {
-					timezone = "GMT-8";
+					timezone = "UTC-8";
 				}
 				if (timezone.toLowerCase().indexOf("pdt") >= 0) {
-					timezone = "GMT-7";
+					timezone = "UTC-7";
 				}
 				if (timezone.toLowerCase().indexOf("est") >= 0) {
-					timezone = "GMT-5";
+					timezone = "UTC-5";
 				}
 				if (timezone.toLowerCase().indexOf("edt") >= 0) {
-					timezone = "GMT-4";
+					timezone = "UTC-4";
 				}
 				if (timezone.toLowerCase().indexOf("cst") >= 0) {
-					timezone = "GMT-6";
+					timezone = "UTC-6";
 				}
 				if (timezone.toLowerCase().indexOf("cdt") >= 0) {
-					timezone = "GMT-5";
+					timezone = "UTC-5";
 				}
 				if (timezone.toLowerCase().indexOf("aest") >= 0) {
-					timezone = "GMT+10";
+					timezone = "UTC+10";
 				}
 				if (timezone.toLowerCase().indexOf("aedt") >= 0) {
-					timezone = "GMT+11";
+					timezone = "UTC+11";
 				}
 
 				timezone = timezone.replace(/ /g, "");
 				var substringBoundry = timezone.length;
-				var timezoneOffsetHours = parseInt(timezone.substring(4,substringBoundry), 10);
-				var convertedHour;
+				console.log("Timezone Infos: " + timezone + ", length " + substringBoundry);
 
-	/*			if (timezone.indexOf("-") >= 0) {
-					convertedHour = hour + timezoneOffsetHours;
-				}
-				if (timezone.indexOf("+") >= 0) {
-					convertedHour = hour - timezoneOffsetHours;
-				}
+				var convertedHour = hour;
+				console.log("Converted Hour 1: " + convertedHour);
 
-				if (convertedHour >= 24) {
-					convertedHour = convertedHour - 24;
-					day++;
+				if (substringBoundry > 4) {
+					var timezoneOffsetHours = timezone.substring(4,substringBoundry);
+					console.log("Timezone Offset Hours String: " + timezoneOffsetHours);
+					timezoneOffsetHours = parseInt(timezoneOffsetHours, 10);
+					console.log("Timezone Offset Hours Integer: " + timezoneOffsetHours);
 
-					var daysInMonth = 31;
-					if ([9,4,6,11].indexOf(month) >=0) {
-						daysInMonth = 30;
+					if (timezone.indexOf("-") >= 0) {
+						convertedHour = hour + timezoneOffsetHours;
 					}
-					if (month == 2) {
-						daysInMonth = 28;
+					if (timezone.indexOf("+") >= 0) {
+						convertedHour = hour - timezoneOffsetHours;
 					}
 
-					if (day > daysInMonth) {
-						day = 1;
-						month++;
+					if (convertedHour >= 24) {
+						convertedHour = convertedHour - 24;
+						day++;
 
-						if (month > 12) {
-							month = 1;
+						var daysInMonth = 31;
+						if ([9,4,6,11].indexOf(month) >=0) {
+							daysInMonth = 30;
 						}
+						if (month == 2) {
+							daysInMonth = 28;
+						}
+
+						if (day > daysInMonth) {
+							day = 1;
+							month++;
+
+							if (month > 12) {
+								month = 1;
+							}
+						}
+				
 					}
-			
-				}*/
+				}
 
-				//Log new base time and timezone
-				//console.log("Converted - " + title + " - " + convertedHour + ":" + minute + " - " + day + "/" + month + "/" + year);
+				console.log("Converted Hour 2: " + convertedHour);
 
-				//getCountdownHref = "http://www.timeanddate.com/scripts/gocountdown.php?theme=text&msg=" + title.replace(/ /g, "+") + "&font=sanserif&month=" + month + "&day=" + day + "&year=" + year + "&hour=" + convertedHour + "&min=" + minute + "&sec=0&p0=%3A&p0txt=" + currentLocation;
+				//Output new UTC time
+				console.log("Converted to UTC: " + title + " - " + convertedHour + ":" + minute + " - " + day + "/" + month + "/" + year);
+
+				var epochFuture = Date.UTC(year,month-1,day,convertedHour,minute);
+				console.log("Future Epoch Before MS: " + epochFuture);
+				epochFuture = Math.floor(epochFuture/1000);
+				//epochFuture = 1440050400;
+				var epochNow = Math.floor(Date.now()/1000);
+				countdowns[i] = epochFuture - epochNow;
 
 				if (title.length > 25) {
-					title = title.substring(0,25) + "...";
+					titleShort = title.substring(0,22) + "...";
+				} else {
+					titleShort = title;
 				}
-				eventsString = eventsString + '<p style="float: left"><strong><a href="' + href + '" target="_blank">'+ title + '</a></strong></p><p style="float: right">' + eventParts[1] + ' @ ' + eventParts[4] + '</p><br /><br />';
+				
+				if (!isNaN(countdowns[i])) {
+					var localDate = new Date(epochFuture*1000);
+					localDate = localDate.toString().substring(0,21);
+					eventsString = eventsString + '<p style="float: left"><strong><a href="' + href + '" target="_blank">'+ titleShort + '</a></p><p style="float: right"><span id="timer' + i + '" style="color:#48a948"></span></strong></p><br /><br /><p style="float: left"><strong>' + currentLocation.replace(/\+/g, " ") + ' Time:</strong></p><p style="float: right">' + localDate + '</p><br /><p align="center"><img src="https://lh3.googleusercontent.com/6Evhp9jZ4ocalVFkHdRWgLkG9XkPrrKT0ATrQN0ruLnQ=w699-h9-no" border=0 width="100%"></p>';
+				} else {
+					eventsString = eventsString + '<p style="float: left"><strong><a href="' + href + '" target="_blank">'+ titleShort + '</a></p><p style="float: right"><span id="timer' + i + '"></span></p></strong><br /><p align="center"><img src="https://lh3.googleusercontent.com/6Evhp9jZ4ocalVFkHdRWgLkG9XkPrrKT0ATrQN0ruLnQ=w699-h9-no" border=0 width="100%"></p>';
+				}
 			}
 		}
-		$(".md").prepend('<blockquote><h3>Upcoming Cruises (' + events.length + ')</h3><div id="upcomingEventText">' + eventsString + '<center><strong>Your timezone location: ' + currentLocation.replace(/\+/g, " ") + '</strong></center></div></blockquote>');
+
+		$(".md").prepend('<blockquote><h3>Upcoming Cruises (' + events.length + ')</h3><div id="upcomingEventText">' + eventsString + '<center><strong>Report Widget Bugs to <a title="All your base are belong to PapaSyntax" href="https://www.reddit.com/user/PapaSyntax/" target="_blank">PapaSyntax</a></strong></center></div></blockquote>');
+
+		for (var i=0; i < events.length; i++) {
+			timerUpdate(i);
+			var funcInterval = setInterval(timerUpdate(i), 1000);
+		}
 	})
 })
