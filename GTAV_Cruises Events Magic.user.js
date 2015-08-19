5e1a9be287f9911b8278a8fee9d2350352d47d62
Justin Howe <justinhowe@outlook.com>
2015-08-19 10:41:26 -0700

    gta

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GTAV_Cruises Events Magic
 // @namespace    https://github.com/JustinHowe/userscripts/
-// @version      1.11
+// @version      1.12
 // @description  Events block for GTAV_Cruises
 // @author       Syntaximus
 // @match        https://www.reddit.com/r/GTAV_Cruises/
@@ -18,6 +18,8 @@ var countdowns = [];
 var dates = [];
 var times = [];
 var zones = [];
+var tooFarOut = false;
+var s,m,h,d;
 
 console.log = function() {} //Comment to enable console logging. 
 
@@ -27,7 +29,7 @@ function toTitleCase(str) {
 
 function timerUpdate(n) {
 	var timerString = "timer" + n;
-	if (!isNaN(countdowns[n])) {
+	if ((!isNaN(countdowns[n])) && !tooFarOut) {
 		s = countdowns[n]%60;
 		m = (countdowns[n]-s)/60%60;
 		h = ((countdowns[n]-s)/60 - m)/60%24;
@@ -219,6 +221,10 @@ $(window).load(function(){
 				//epochFuture = 1440050400;
 				var epochNow = Math.floor(Date.now()/1000);
 				countdowns[i] = epochFuture - epochNow;
+				ss = countdowns[i]%60;
+				mm = (countdowns[i]-s)/60%60;
+				hh = ((countdowns[i]-s)/60 - m)/60%24;
+				dd = (((countdowns[i]-s)/60 - m)/60 - h)/24;
 
 				if (title.length > 25) {
 					titleShort = title.substring(0,22) + "...";
@@ -226,12 +232,12 @@ $(window).load(function(){
 					titleShort = title;
 				}
 				
-				if (!isNaN(countdowns[i])) {
+				if ((!isNaN(countdowns[i])) && (dd <= 21)) {
 					var localDate = new Date(epochFuture*1000);
 					localDate = localDate.toString().substring(0,21);
 					eventsString = eventsString + '<p style="float: left"><strong><a title="Link to: ' + title + '" href="' + href + '" target="_blank">'+ titleShort + '</a></p><p style="float: right"><span id="timer' + i + '" style="color:#48a948"></span></strong></p><br /><br /><p style="float: left"><strong>Local Time:</strong></p><p style="float: right"><strong><font size="1">' + localDate + '</font></strong></p><br /><p align="center"><img src="https://lh3.googleusercontent.com/6Evhp9jZ4ocalVFkHdRWgLkG9XkPrrKT0ATrQN0ruLnQ=w699-h9-no" border=0 width="100%"></p>';
 				} else {
-					eventsString = eventsString + '<p style="float: left"><strong><a title="No Countdown Timer - Bad Date - Should be day/month/year. err_code:id10t" href="' + href + '" target="_blank">'+ titleShort + '</a></p><p style="float: right"><span id="timer' + i + '"></span></p></strong><br /><p align="center"><img src="https://lh3.googleusercontent.com/6Evhp9jZ4ocalVFkHdRWgLkG9XkPrrKT0ATrQN0ruLnQ=w699-h9-no" border=0 width="100%"></p>';
+					eventsString = eventsString + '<p style="float: left"><strong><a title="No Countdown Timer - Bad Date or Too Far in Future - Follow day/month/year format in event titles." href="' + href + '" target="_blank">'+ titleShort + '</a></p><p style="float: right"><span id="timer' + i + '"></span></p></strong><br /><p align="center"><img src="https://lh3.googleusercontent.com/6Evhp9jZ4ocalVFkHdRWgLkG9XkPrrKT0ATrQN0ruLnQ=w699-h9-no" border=0 width="100%"></p>';
 				}
 			}
 		}
