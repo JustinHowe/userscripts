1eea487c0b8f18db396e776bc1939df002bef98c
Justin Howe <justinhowe@outlook.com>
2015-08-20 13:25:57 -0700

    gta

--- GTAV_Cruises Events Magic.user.js
+++ GTAV_Cruises Events Magic.user.js
@@ -22,7 +22,7 @@ var day = "d";
 var month = "m";
 var year = "y";
 
-console.log = function() {} //Comment to enable console logging. 
+//console.log = function() {} //Comment to enable console logging. 
 
 function toTitleCase(str) {
 	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
