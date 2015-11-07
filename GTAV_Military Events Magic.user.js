7c28d3e48e00745ec067cc85687a12cd1c1aabe2
Justin Howe <justinhowe@outlook.com>
2015-11-07 01:05:29 -0800

    update
    update

--- GTAV_Military Events Magic.user.js
+++ GTAV_Military Events Magic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GTAV_Military Events Magic
 // @namespace    https://github.com/JustinHowe/userscripts/
-// @version      1.95
+// @version      1.97
 // @description  Events block for GTAV_Military
 // @author       Syntaximus
 // @match        https://www.reddit.com/r/GTAV_Military
